b709d0e48c71bdd00c36c65c20b6ead6a8b5ea48
sselea <sselea88@gmail.com>
2014-06-24 09:32:40 -0400

    Creating Log In Functionality

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+  user= User.find_by_email (params[:email])
+  if user && user.autheticate(params [:password])
+  	sessions[:user_id]= user.id
+  	redirect_to products_urlm, :notice => "Logged In!"
+  else
+  	render "new"
+  end
+  end
+
+
+  def destroy
+  	session[:user_id]= nil
+  	redirect_to products_url, :notce => "Logged Out"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 	resources :products
 	resources :users, :only => [:new,:create] 
+	resources :sessions, :only => [:new, :create, :destroy]
 end
 
   
